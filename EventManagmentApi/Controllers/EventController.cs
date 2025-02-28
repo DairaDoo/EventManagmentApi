e4858f6eedec2cae639291679c661ca5fb36653d
DairanDoo <your_email@example.com>
2025-02-27 20:07:57 -0400

    Controlador finalizado.

--- EventManagmentApi/Controllers/EventController.cs
+++ EventManagmentApi/Controllers/EventController.cs
@@ -1,6 +1,67 @@
-﻿namespace EventManagmentApi.Controllers
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EventManagmentApi.Models;
+using EventManagmentApi.Service;
+
+namespace EventManagmentApi.Controllers
 {
-    public class EventController
+    [ApiController]
+    [Route("api/events")] // ruta de la api
+    public class EventController: ControllerBase
     {
+        private readonly EventService _eventService;
+
+        // Injectamos el EventService para acceder a la lógica de negocio.
+        public EventController(EventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        // Get All Events
+        [HttpGet]
+        public async Task<IActionResult> GetAllEvents()
+        {
+            var events = await _eventService.GetAllEventsAsync();
+            return Ok(events);
+        }
+
+        // Get Event By ID
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEventById(int id)
+        {
+            var evt = await _eventService.GetEventByIdAsync(id); // Get Event
+            if (evt == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(evt);
+        }
+
+        // Create Event
+        [HttpPost]
+        public async Task<IActionResult> CreateEvent(Event evt)
+        {
+            var newEventId = await _eventService.CreateEventAsync(evt);
+            return CreatedAtAction(nameof(GetEventById), new {id = newEventId}, evt);
+        }
+
+        [HttpPut("{id}")] 
+        public async Task<IActionResult> UpdateEvent(int id, Event evt)
+        {
+            if (id != evt.Id) return BadRequest();
+            var updatedEvent = await _eventService.UpdateEventAsync(evt);
+            return updatedEvent ? NoContent() : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvent(int id)
+        {
+            var deletedEvent = await _eventService.DeleteEventAsync(id);
+            return deletedEvent ? NoContent() : NotFound();
+        }
+
+
     }
 }
